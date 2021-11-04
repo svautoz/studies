7f67c5b707b78032f5fc57b7d52b53151c183e77
User <C:\Users\User\AppData\Roaming\The Bat!>
2021-11-04 12:09:55 +0300

    Git init

---
+++ dog/Program.cs
@@ -0,0 +1,27 @@
+﻿double distance = 200;
+int count = 0;
+int speed_1 = 2;
+int speed_2 = 3;
+int dog_speed = 5;
+int direction = 2;
+double time = 0;
+
+while(distance > 10)
+{
+    if(direction == 2)
+    {
+        time = distance / (speed_2 + dog_speed);
+        direction = 1;
+    }
+    else
+    {
+        time = distance / (speed_1 + dog_speed);        
+        direction = 2;
+    }
+    distance = distance - time * (speed_2 + speed_1);
+    count++;
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine(" раз.");
---
+++ example_5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя:");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
