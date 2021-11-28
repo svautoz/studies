d078190bd974d1796ca4b0dbb56c9e5fa882541e
User <C:\Users\User\AppData\Roaming\The Bat!>
2021-11-28 20:24:08 +0300

    HW8 in progress

--- SEM8/Program.cs
+++ SEM8/Program.cs
@@ -21,6 +21,21 @@ void PrintArray(int[] array)
     System.Console.WriteLine();
 }
 
+void PrintArrayWithIndex(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(i + " ");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+
 
 int[,] MakeRandomArray(int rows, int cols, int min, int max)
 {
@@ -151,13 +166,148 @@ PrintMatrix(SortRowsInMatrix(array));
 
 // 58. Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить
 System.Console.WriteLine("58. Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить");
+
+int[,] TransponSquareMatrix(int[,] matrix)
+{
+    int temp;
+    if (matrix.GetLength(0) != matrix.GetLength(1))
+    {
+        System.Console.WriteLine("Матрица не квадратная");
+        return matrix;
+    }
+    for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+    {
+        for (int col = row + 1; col < matrix.GetLength(1); col++)
+        {
+            temp = matrix[row, col];
+            matrix[row, col] = matrix[col, row];
+            matrix[col, row] = temp;
+        }
+    }
+    return matrix;
+}
+
+int[,] TransponMatrix(int[,] matrix)
+{
+    int[,] transponMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            transponMatrix[col, row] = matrix[row, col];
+        }
+    }
+    return transponMatrix;
+}
+
+int[,] matrix = MakeRandomArray(5, 5, 0, 100);
+PrintMatrix(matrix);
+PrintMatrix(TransponSquareMatrix(matrix));
+
+matrix = MakeRandomArray(5, 10, 0, 100);
+PrintMatrix(matrix);
+PrintMatrix(TransponMatrix(matrix));
+
 // 59. В прямоугольной матрице найти строку с наименьшей суммой элементов.
 System.Console.WriteLine("59. В прямоугольной матрице найти строку с наименьшей суммой элементов.");
+int GetMinSumValuesRow(int[,] matrix)
+{
+    int minRowSum = 0;
+    int minRowIndex = 0;
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        minRowSum += matrix[0, col];
+    }
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        int rowSum = 0;
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            rowSum += matrix[row, col];
+        }
+        if (rowSum < minRowSum)
+        {
+            minRowSum = rowSum;
+            minRowIndex = row;
+        }
+    }
+    return minRowIndex;
+}
+
+matrix = MakeRandomArray(5, 4, 0, 10);
+PrintMatrix(matrix);
+System.Console.WriteLine(GetMinSumValuesRow(matrix));
+
 // 60. Составить частотный словарь элементов двумерного массива
 System.Console.WriteLine("60. Составить частотный словарь элементов двумерного массива");
+int[] GetFreqDictionary(int[,] matrix, int minValue, int maxValue)
+{
+    int[] freqDictionary = new int[maxValue - minValue + 1];
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            freqDictionary[matrix[row, col]] += 1;
+        }
+    }
+    return freqDictionary;
+}
+
+int minValue = 0, maxValue = 10;
+matrix = MakeRandomArray(5, 4, minValue, maxValue);
+PrintMatrix(matrix);
+PrintArrayWithIndex(GetFreqDictionary(matrix, minValue, maxValue));
+
+
 // 61. Найти произведение двух матриц
+System.Console.WriteLine("61. Найти произведение двух матриц");
+
+int[,] GetMultipliedMatrix(int[,] matrixA, int[,] matrixB)
+{
+    if (matrixA.GetLength(1) != matrixB.GetLength(0))
+    {
+        System.Console.WriteLine("Невозможно перемножить");
+        return new int[,] { };
+    }
+
+    int newMatrixRows, newMatrixCols;
+    if (matrixB.GetLength(0) > matrixA.GetLength(0)) newMatrixRows = matrixB.GetLength(0);
+    else newMatrixRows = matrixA.GetLength(0);
+    if (matrixB.GetLength(1) > matrixA.GetLength(1)) newMatrixCols = matrixB.GetLength(1);
+    else newMatrixCols = matrixA.GetLength(1);
+
+    int[,] matrixC = new int[newMatrixRows, newMatrixCols];
+    for (int row = 0; row < newMatrixRows; row++)
+    {
+        for (int col = 0; col < newMatrixCols; col++)
+        {
+            matrixC[row, col] = GetRowsMultiply(row, col, matrixA, matrixB);
+        }
+    }
+    return matrixC;
+}
+
+int GetRowsMultiply(int row, int col, int[,] matrixA, int[,] matrixB)
+{
+    int result = 0;
+    for (int i = 0; i < matrixA.GetLength(1); i++)
+    {
+        result += matrixA[row, i] * matrixB[i, col];
+    }
+    return result;
+}
+
+int[,] matrixA = MakeRandomArray(3, 2, 0, 10);
+PrintMatrix(matrixA);
+int[,] matrixB = MakeRandomArray(2, 4, 0, 10);
+PrintMatrix(matrixB);
+PrintMatrix(GetMultipliedMatrix(matrixA, matrixB));
+
+
 // 62. В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+System.Console.WriteLine("62. В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.");
 // 63. Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+System.Console.WriteLine("63. Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента");
 // 64. Показать треугольник Паскаля
 // *Сделать вывод в виде равнобедренного треугольника
 // 65. Спирально заполнить двумерный массив:
